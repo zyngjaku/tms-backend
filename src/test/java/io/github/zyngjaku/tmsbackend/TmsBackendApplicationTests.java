4fb355a8b3431759276b58ddd3af56557aa9833f
zyngjaku <zyngi98@gmail.com>
2020-11-20 09:10:16 +0100

    Init spring project

---
+++ src/test/java/io/github/zyngjaku/tmsbackend/TmsBackendApplicationTests.java
@@ -0,0 +1,9 @@
+package io.github.zyngjaku.tmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsBackendApplicationTests {
+
+}
