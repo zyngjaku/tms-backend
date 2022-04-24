d028b0a8b041f359fb127bf42e9cff205d9cfa76
SG0309904 <jakub.zygier.ctr@sabre.com>
2022-04-24 20:58:14 +0200

    Added simple test

---
+++ src/test/java/io/github/zyngjaku/tmsbackend/SimpleTests.java
@@ -0,0 +1,12 @@
+package io.github.zyngjaku.tmsbackend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleTests {
+    @Test
+    public void simpleTest() {
+        assertThat(1).isEqualTo(1);
+    }
+}
