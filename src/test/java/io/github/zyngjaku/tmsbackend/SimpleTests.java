a3fd459803de64e3457c7c18c5ad840ef52c5d45
SG0309904 <jakub.zygier.ctr@sabre.com>
2022-04-24 21:28:45 +0200

    Added tests

--- src/test/java/io/github/zyngjaku/tmsbackend/SimpleTests.java
+++ src/test/java/io/github/zyngjaku/tmsbackend/SimpleTests.java
@@ -1,12 +1,50 @@
 package io.github.zyngjaku.tmsbackend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SimpleTests {
     @Test
-    public void simpleTest() {
+    public void passSimpleTest() {
         assertThat(1).isEqualTo(1);
+    }
+
+    @Test
+    public void passSimpleTest2() {
+        assertThat(2).isEqualTo(2);
+    }
+
+    @Test
+    public void passSimpleTest3() {
+        assertThat(3).isEqualTo(3);
+    }
+
+    @Test
+    public void passSimpleTest4() {
+        assertThat(4).isEqualTo(4);
+    }
+
+    @Test
+    public void passSimpleTest5() {
+        assertThat(5).isEqualTo(5);
+    }
+
+    @Test
+    public void passFailTest() {
+        assertThat(1).isEqualTo(2);
+    }
+
+    @Test
+    @Disabled("Ignore tests, not ready")
+    public void simpleSkipTest() {
+        assertThat(1).isEqualTo(1);
+    }
+
+    @Test
+    @Disabled("Ignore tests, not ready")
+    public void simpleSkipTest2() {
+        assertThat(1).isEqualTo(2);
     }
 }
